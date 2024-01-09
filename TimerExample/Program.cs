07308a27848b9dce7257c56e2f1ec7d457e6f28f
Muhammad-Ohee <md.ohee86@gmail.com>
2024-01-09 19:05:36 +0600

    Add project files.

---
+++ TimerExample/Program.cs
@@ -0,0 +1,41 @@
+﻿//Console.WriteLine("Hello, World!");
+using System.Runtime.ConstrainedExecution;
+using Timer = System.Timers.Timer;
+
+namespace TimerSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer myTimer = new Timer(2000);
+
+            myTimer.Elapsed += MyTimer_Elapsed;
+            myTimer.Elapsed += MyTimer_Elapsed1;
+
+            myTimer.Start();
+
+            Console.WriteLine("Press enter to remove the green event.");
+
+            Console.ReadLine();
+
+            myTimer.Elapsed -= MyTimer_Elapsed1;
+
+            Console.ReadLine();
+        }
+
+        private static void MyTimer_Elapsed1(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Elapsed: {0:HH:mm:ss.fff}", e.SignalTime);
+            //throw new NotImplementedException();
+        }
+
+        private static void MyTimer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            Console.ForegroundColor= ConsoleColor.Blue;
+            Console.WriteLine("Elapsed: {0:HH:mm:ss.fff}", e.SignalTime);
+            //throw new NotImplementedException();
+        }
+    }
+}
